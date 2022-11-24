3658b5310b3bf830ab9f742c0385f434521f5e59
Naveenalla3000 <111577927+Naveenalla3000@users.noreply.github.com>
2022-11-24 23:57:08 +0530

    Update sumofnumbers.java

--- sumofnumbers.java
+++ sumofnumbers.java
@@ -4,3 +4,16 @@ class sumOfNumbers {
         System.out.println("sum "+( a+b));
     }
 }
+
+//---------------------------------------------------------//
+import java.util.*;
+class sumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int x = scn.nextInt();
+        System.out.print("Enter the second number :");
+        int y= scn.nextInt();
+        System.out.println("sum "+(x+y));
+    }
+}
