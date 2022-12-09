14e22b22eb46a21a1ab55318e3c55229553dcaad
Naveenalla3000 <111577927+Naveenalla3000@users.noreply.github.com>
2022-12-09 16:34:30 +0530

    Create classes.java

---
+++ classes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class hello{
+    public static void main(String[] args){
+        student std= new student();
+        Scanner scr = new Scanner(System.in);
+          System.out.print("Enter the name : ");
+        std.name=scr.nextLine();
+        System.out.print("Enter the id : ");
+        std.id=scr.nextInt();
+      
+        System.out.println(std.id+"\n"+std.name);
+    }
+}
+class student{
+    int id;
+    String name;
+}
